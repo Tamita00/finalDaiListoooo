93466137333b040f491a8c5ec5d4ca27074ee228
Android Studio <devandroid@ort>
2024-10-28 16:27:27 -0300

    j

--- api/apiManager.js
+++ api/apiManager.js
@@ -1,9 +1,9 @@
 import axios from "axios";
-
+  
 const baseURL = "http://localhost:3000/api";
 
 const apiManager = async (method, headers, data, path) => {
-  console.log('Headers:', headers);
+  console.log('Headers:', headers);  
   try {
     const response = await axios({
       method: method,
@@ -11,11 +11,11 @@ const apiManager = async (method, headers, data, path) => {
       data: data,
       headers: headers,
     });
-    console.log('Response:', response.data);
-    return response.data; // Devuelve solo los datos de la respuesta
+    console.log('Response:', response);  
+    return response;  
   } catch (error) {
-    console.error(`Error en la solicitud ${method} a ${path}: ${error.message}`);
-    throw new Error(`Error en la solicitud ${method} a ${path}: ${error.message}`);
+    console.error(`Error en la solicitud POST a ${path}: ${error.message}`);
+    throw new Error(`Error en la solicitud POST a ${path}: ${error.message}`);
   }
 };
 
